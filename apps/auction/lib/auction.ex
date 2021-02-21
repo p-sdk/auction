a6da01c1dd85e34fb362c330cd62b470b3e08c00
p-sdk <paul.sdk@icloud.com>
2021-02-21 22:06:17 +0100

    Handle user authentication

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,9 +1,18 @@
 defmodule Auction do
-  alias Auction.{Repo, Item, User}
+  alias Auction.{Repo, Item, User, Password}
 
   @repo Repo
 
   def get_user(id), do: @repo.get!(User, id)
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
+         true <- Password.verify_with_hash(password, user.hashed_password) do
+      user
+    else
+      _ -> Password.dummy_verify()
+    end
+  end
 
   def new_user, do: User.changeset_with_password(%User{})
 
