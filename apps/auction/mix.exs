7fa1446ba35607b2ef5b0d9301ac18430b0b458d
p-sdk <paul.sdk@icloud.com>
2021-02-25 23:12:37 +0100

    Add missing phoenix_pubsub to deps

--- apps/auction/mix.exs
+++ apps/auction/mix.exs
@@ -29,6 +29,7 @@ defmodule Auction.MixProject do
       {:ecto_sql, "~> 3.5"},
       {:postgrex, "~> 0.15.8"},
       {:pbkdf2_elixir, "~> 1.3"},
+      {:phoenix_pubsub, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
