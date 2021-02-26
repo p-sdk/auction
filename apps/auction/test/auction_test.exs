37c29c167fc12347fce29fc061201175fa2435b5
p-sdk <paul.sdk@icloud.com>
2021-02-26 14:33:21 +0100

    Test inserting an item

--- apps/auction/test/auction_test.exs
+++ apps/auction/test/auction_test.exs
@@ -1,5 +1,6 @@
 defmodule AuctionTest do
   use ExUnit.Case
+  import Ecto.Query
   alias Auction.{Item, Repo}
   doctest Auction
 
@@ -30,6 +31,26 @@ defmodule AuctionTest do
     test "returns a single Item based on id", %{items: items} do
       item = Enum.at(items, 1)
       assert item == Auction.get_item(item.id)
+    end
+  end
+
+  describe "insert_item/1" do
+    test "adds an Item to the database" do
+      count_query = from(i in Item, select: count(i.id))
+      before_count = Repo.one(count_query)
+      {:ok, _item} = Auction.insert_item(%{title: "test item"})
+      assert Repo.one(count_query) == before_count + 1
+    end
+
+    test "the Item in the database has the attributes provided" do
+      attrs = %{title: "test item", description: "test description"}
+      {:ok, item} = Auction.insert_item(attrs)
+      assert item.title == attrs.title
+      assert item.description == attrs.description
+    end
+
+    test "it returns an error on error" do
+      assert {:error, _changeset} = Auction.insert_item(%{foo: :bar})
     end
   end
 end
