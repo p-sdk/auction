116a68f548094d6951a80cf2e87e75ad1c8dbd38
p-sdk <paul.sdk@icloud.com>
2021-02-26 14:18:24 +0100

    Test retrieving items

--- apps/auction/test/auction_test.exs
+++ apps/auction/test/auction_test.exs
@@ -1,13 +1,35 @@
 defmodule AuctionTest do
   use ExUnit.Case
-  alias Auction.{Repo}
+  alias Auction.{Item, Repo}
   doctest Auction
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
   end
 
-  test "the truth" do
-    assert true
+  describe "list_items/0" do
+    setup do
+      {:ok, item1} = Repo.insert(%Item{title: "Item 1"})
+      {:ok, item2} = Repo.insert(%Item{title: "Item 2"})
+      {:ok, item3} = Repo.insert(%Item{title: "Item 3"})
+      %{items: [item1, item2, item3]}
+    end
+
+    test "returns all Items in the database", %{items: items} do
+      assert items == Auction.list_items()
+    end
+  end
+
+  describe "get_item/1" do
+    setup do
+      {:ok, item1} = Repo.insert(%Item{title: "Item 1"})
+      {:ok, item2} = Repo.insert(%Item{title: "Item 2"})
+      %{items: [item1, item2]}
+    end
+
+    test "returns a single Item based on id", %{items: items} do
+      item = Enum.at(items, 1)
+      assert item == Auction.get_item(item.id)
+    end
   end
 end
